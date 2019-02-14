aba7776b45cdfd1e53a1d4a5b7b22088c5222491
Yaroslav Golinskiy <kaiserdem@gmail.com>
2019-02-14 13:10:38 +0200

    added navigation down view and spinner

--- Show City/Controller/MapVC.swift
+++ Show City/Controller/MapVC.swift
@@ -15,10 +15,17 @@ class MapVC: UIViewController, UIGestureRecognizerDelegate {
   @IBOutlet weak var mapView: MKMapView!
   
   @IBOutlet weak var pullUpView: UIView!
-  @IBOutlet weak var mapViewBottomConstraints: NSLayoutConstraint!
+  
+  @IBOutlet weak var pullUpViewHightConstraints: NSLayoutConstraint!
+  
   var locationManager = CLLocationManager()
   let authorizationStatus = CLLocationManager.authorizationStatus()
   let regionRadius: Double = 1000
+  
+  var spinner: UIActivityIndicatorView?
+  var progressLbl: UILabel?
+  
+  var screenSize = UIScreen.main.bounds
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -36,13 +43,34 @@ class MapVC: UIViewController, UIGestureRecognizerDelegate {
     doubleTap.delegate = self
     mapView.addGestureRecognizer(doubleTap)
   }
-  func animateViewUp() {
-    mapViewBottomConstraints.constant = 300
+  
+  func addSwipe() { // вю скрываеться
+    let swipe = UISwipeGestureRecognizer(target: self, action: #selector(animateViewDown))
+    swipe.direction = .down
+    pullUpView.addGestureRecognizer(swipe)
+  }
+  func animateViewUp() { // появляеться вю
+    pullUpViewHightConstraints.constant = 300
     UIView.animate(withDuration: 0.3) {
       self.view.layoutIfNeeded()
     }
   }
   
+  @objc func animateViewDown() {
+    pullUpViewHightConstraints.constant = 0
+    UIView.animate(withDuration: 0.3) {
+      self.loadViewIfNeeded()
+    }
+  }
+  
+  func addSpinner() {
+    spinner = UIActivityIndicatorView()
+    spinner?.center = CGPoint(x: (screenSize.width / 2) - ((spinner?.frame.width)! / 2), y: 150)
+    spinner?.style = .white
+    spinner?.color = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+    spinner?.startAnimating()
+    pullUpView.addSubview(spinner!)
+  }
   @IBAction func centerMapBtnWasPressed(_ sender: Any) {
     if authorizationStatus == .authorizedAlways || authorizationStatus == .authorizedWhenInUse {
       centerMapOnUserLocation()
@@ -62,6 +90,7 @@ extension MapVC: MKMapViewDelegate {
     pinAnotation.animatesDrop = true
     return pinAnotation
   }
+  
   func centerMapOnUserLocation() {
     guard let coordinate = locationManager.location?.coordinate else { return }
     
@@ -74,6 +103,9 @@ extension MapVC: MKMapViewDelegate {
     
     removePin()
     animateViewUp()
+    addSwipe()
+    addSpinner()
+    
        // координаты места на карте
     let touchPoint = sender.location(in: mapView)
     let touchCoordinate = mapView.convert(touchPoint, toCoordinateFrom: mapView)
